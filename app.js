76c6754134088f3365c39fbcbfda9d1351a4e133
David Tenni <davidctenni@gmail.com>
2021-09-28 14:01:36 +1000

    refactored: simplified overly complex code

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+const fortunes = ["You will live a long life, unfortunately it will be in a coma", "", "", "", "", "", "", ""];
 
-main = () => {
-    class TerribleEightBall {
-      constructor(fortuneList) {
-        this.name = "Terrible 8 Ball";
-        this.fortunes = ["You will live a long life, unfortunately it will be in a coma", "", "", "", "", "", "", ""];
-        this.drawLuckyNumbers();
-      }
-      tellFortune() {
-        console.log(this.fortunes[0])
-      }
-    }
+
+
+
+const tellFortune = () => {
+  console.log(fortunes[0]);
 }
+
+
+const fortuneButton = document.getElementById("fortune-button");
+fortuneButton.addEventListener("click", tellFortune())
+
+
+
+
+
